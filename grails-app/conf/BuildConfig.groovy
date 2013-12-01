ea4dc34857d2954cc288fbabf87c23fc0dfdc736
Goran Ehrsson <goran@technipelago.se>
2013-12-01 21:34:34 +0100

    Upgraded codenarc to 0.19 and code-coverage to 1.2.7

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -30,8 +30,8 @@ grails.project.dependency.resolution = {
             export = false
             exclude "spock-grails-support"
         }
-        test(":codenarc:0.18.1") { export = false }
-        test(":code-coverage:1.2.6") { export = false }
+        test(":codenarc:0.19") { export = false }
+        test(":code-coverage:1.2.7") { export = false }
 
         compile "grails.crm:crm-core:latest.integration"
         compile "grails.crm:crm-contact:latest.integration"
@@ -47,12 +47,12 @@ grails.project.dependency.resolution = {
 codenarc {
     reports = {
         CrmXmlReport('xml') {
-            outputFile = 'target/test-reports/CodeNarcReport.xml'
-            title = 'Grails CRM CodeNarc Report'
+            outputFile = 'target/CodeNarcReport.xml'
+            title = 'GR8 CRM CodeNarc Report'
         }
         CrmHtmlReport('html') {
-            outputFile = 'target/test-reports/CodeNarcReport.html'
-            title = 'Grails CRM CodeNarc Report'
+            outputFile = 'target/CodeNarcReport.html'
+            title = 'GR8 CRM CodeNarc Report'
 
         }
     }
