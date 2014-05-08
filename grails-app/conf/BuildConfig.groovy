dc448e6e37d4ecfa179a5514f92fcbcb0b2ce622
Goran Ehrsson <goran@technipelago.se>
2014-05-08 22:49:15 +0200

    Added back grails.project.class.dir

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,3 +1,6 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
 grails.project.work.dir = "target"
 grails.project.target.level = 1.6
 
