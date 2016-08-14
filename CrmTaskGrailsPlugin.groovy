9dfac41761d7799d017622fa97e90b7cc2deb66f
Goran Ehrsson <goran@technipelago.se>
2016-08-14 20:37:51 +0200

    Version 2.4.4

--- CrmTaskGrailsPlugin.groovy
+++ CrmTaskGrailsPlugin.groovy
@@ -18,7 +18,7 @@ import grails.plugins.crm.task.CrmTask
 
 class CrmTaskGrailsPlugin {
     def groupId = ""
-    def version = "2.4.4-SNAPSHOT"
+    def version = "2.4.4"
     def grailsVersion = "2.4 > *"
     def dependsOn = [:]
     def loadAfter = ['crmTags']
